731e401541147030dcc59d65acabcf6507135691
nachohotz <juanhotz611@gmail.com>
2022-09-22 23:39:50 -0300

    FIX: hotspot

--- docker-node-mongo/src/app.js
+++ docker-node-mongo/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(helmet.hidePoweredBy());
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
